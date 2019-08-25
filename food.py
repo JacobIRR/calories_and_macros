927651bce36de5ac7b68de3e0f9825c7c86c51b9
Jacob Franklin <jackobefranklin@gmail.com>
2019-08-25 16:49:59 -0700

    fix 100g serving size bug

--- food.py
+++ food.py
@@ -6,30 +6,36 @@ from food_ids import FOOD_IDS, NUTRIENT_IDS, API_KEY
 
 # NOTE: type b is basic, type f is full, s is stats
 URL = 'https://api.nal.usda.gov/ndb/V2/reports?ndbno={}&type=s&format=json&api_key={}'
-
+DEFAULT_SERVING = 100  # grams
 
 class Food(object):
     """a food object from an ndbno ID """
     def __init__(self, ndbno):
         self.id = ndbno
-        self.macros = self.fetch_food_info()
+        self.macros = self._fetch_food_info()
 
-    def fetch_food_info(self):
+    def _fetch_food_info(self):
         resp = requests.get(URL.format(self.id, API_KEY))
         json_dict = resp.json()
+        print("============BEGIN RESPONSE:===============")
         pprint(json_dict)
-        # TODO: deal with serving size, qty also from this response
+        print("============END RESPONSE:===============")
         nutrients = json_dict['foods'][0]['food']['nutrients']
+        serving_grams = nutrients[0]['measures'][0]['eqv']
+        assert nutrients[0]['measures'][0]['eunit'] == 'g'
         # now build the macros
         protein = fat = carbs = 0
         for d in nutrients:
             if d['nutrient_id'] == NUTRIENT_IDS['protein']:
-                protein = float(d['value'])
+                protein = self._calc_calorie_for_macro(serving_grams, float(d['value']))
             elif d['nutrient_id'] == NUTRIENT_IDS['fat']:
-                fat = float(d['value'])
+                fat = self._calc_calorie_for_macro(serving_grams, float(d['value']))
             elif d['nutrient_id'] == NUTRIENT_IDS['carbs']:
-                carbs = float(d['value'])
+                carbs = self._calc_calorie_for_macro(serving_grams, float(d['value']))
         return Macros(protein, fat, carbs)
+
+    def _calc_calorie_for_macro(self, serving_size, value):
+        return round(serving_size / DEFAULT_SERVING * value, 1)
 
 
 
